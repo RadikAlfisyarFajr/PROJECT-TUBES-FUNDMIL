23dce28135bc5a4b678ed962ef7f3d5d1ccc678c
HabibAyamKecap <habibkm10@gmail.com>
2026-05-04 13:49:38 +0700

    membuat tampilan laporan

--- resources/views/admin/laporan/laporan-create.blade.php
+++
@@ -1,6 +0,0 @@
-@include('admin.partials.simple-page', [
-    'title' => 'Tambah Laporan',
-    'description' => 'Form tambah laporan memakai sidebar dan tema admin instansi yang sama.',
-    'active' => 'laporan',
-    'backRoute' => 'laporan.index',
-])
--- resources/views/admin/laporan/laporan-show.blade.php
+++
@@ -1,6 +0,0 @@
-@include('admin.partials.simple-page', [
-    'title' => 'Detail Laporan',
-    'description' => 'Detail laporan memakai sidebar dan tema admin instansi yang sama.',
-    'active' => 'laporan',
-    'backRoute' => 'laporan.index',
-])
