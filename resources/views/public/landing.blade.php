94990b0c3197be57d8d3ba594f98063953a2db56
firdaus03-max <m.ridwan.firdaus03@gmail.com>
2026-06-10 21:43:49 +0700

    update new

--- resources/views/public/landing.blade.php
+++ resources/views/public/landing.blade.php
@@ -46,8 +46,8 @@ $instansiComparison = collect($instansiComparison ?? []);
             text-decoration: none;
         }
 
-        .brand-lockup {
-            width: clamp(210px, 26vw, 286px);
+        .brand-logo {
+            width: clamp(180px, 20vw, 270px);
             height: auto;
             display: block;
         }
@@ -473,7 +473,7 @@ $instansiComparison = collect($instansiComparison ?? []);
     <nav class="site-nav sticky-top">
         <div class="container d-flex align-items-center justify-content-between py-3">
             <a class="brand" href="{{ route('public.home') }}">
-                <img class="brand-lockup" src="{{ asset('assets/fundmil-lockup.svg') }}" alt="FUNDMIL SOREANG">
+                <img class="brand-logo" src="{{ asset('assets/logo_amil.png') }}" alt="Logo Amil">
             </a>
             <div class="nav gap-4">
                 <a class="nav-link active px-0" href="#beranda">Beranda</a>
